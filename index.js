a2db79437129027b1c565bf22cbbb1ce1fc3258b
Daniel <dandesantis14@gmail.com>
2021-09-30 18:04:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+function clickAlert() {
+    alert('teehee!');
+}
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert);
 }
